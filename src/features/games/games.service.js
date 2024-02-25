8dc6f9623661599e524652a9ddf07c9cffd730a1
Ossama kharbaq <ossamakharbaq4@gmail.com>
2024-02-25 20:18:26 +0100

    Remove deprecated games service functionality

--- src/features/games/games.service.js
+++
@@ -1,9 +0,0 @@
-import api from "../../api";
-
-const fetchGames = async (leagueId) => {
-    const response = await api.get(`/fixtures?include=participants;scores&league_id=${leagueId}`);
-    return response.data.data;
-};
-
-const gamesService = {fetchGames}
-export default gamesService
