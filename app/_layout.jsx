50b90713a50bf913f5690ccf069ef6031f96088b
Ossama kharbaq <ossamakharbaq4@gmail.com>
2024-02-25 20:18:26 +0100

    Optimize layout component for better responsiveness

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,12 +1,14 @@
 import { Stack } from "expo-router";
 import Navigation from "../components/Navigation";
+import { Provider } from "react-redux";
+import { store } from "../src/store";
 
 const Layout = () => {
   return (
-    <>
+    <Provider store={store}>
       <Stack />
       <Navigation/>
-    </>
+    </Provider>
   );
 };
 export default Layout;
