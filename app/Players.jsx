30111c179c03dbbbab3efc9cae7c65adc6bc7df0
Ossama kharbaq <ossamakharbaq4@gmail.com>
2024-02-26 13:57:08 +0100

    Refactor and update code in Players component

--- app/Players.jsx
+++ app/Players.jsx
@@ -1,46 +1,15 @@
-import axios from "axios";
-import { Stack, useRouter } from "expo-router";
-import { useEffect, useState } from "react";
+import { Stack } from "expo-router";
+import {  useState } from "react";
 import {
-  FlatList,
   Image,
-  ScrollView,
   Text,
   TextInput,
   TouchableOpacity,
   View,
 } from "react-native";
-
+import PlayersContainer from "../components/players"
 const Players = () => {
-  const router = useRouter();
-  const [players, setPlayers] = useState([]);
-  const [playersSearched, setPlayersSearched] = useState([]);
   const [searchStatement, setSearchStatement] = useState("");
-  useEffect(() => {
-    axios
-      .get(
-        "https://api.sportmonks.com/v3/football/players?include=country;position",
-        {
-          headers: {
-            Authorization:
-              "tYgX0otkxc857iGQk2dAVFYOiNCNGi9Qr2sUH40UVpHphNDjdeIdXvrRwb4I",
-          },
-        }
-      )
-      .then((response) => {
-        console.log(response.data.data[0].position.name);
-        setPlayers(response.data.data);
-      });
-  }, []);
-  useEffect(() => {
-    if (searchStatement != "") {
-      console.log(searchStatement);
-      let searchResult = players.filter((item) =>
-        item.name.toLowerCase().includes(searchStatement.toLowerCase())
-      );
-      setPlayersSearched(searchResult);
-    }
-  }, [searchStatement]);
   return (
     <View
       style={{
@@ -95,136 +64,7 @@ const Players = () => {
           />
         </TouchableOpacity>
       </View>
-      <ScrollView style={{ padding: 24 }}>
-        {searchStatement == "" ? (
-          <>
-            <FlatList
-              data={players}
-              renderItem={({ item }) => (
-                <TouchableOpacity onPress={()=>{
-                    router.push(`player-details/${item.id}`)
-                }} style={{ marginBottom: 12 }}>
-                  <View
-                    style={{
-                      backgroundColor: "black",
-                      padding: 16,
-                      borderRadius: 12,
-                      flexDirection: "row",
-                      alignItems: "center",
-                      gap: 8,
-                    }}
-                  >
-                    <Image
-                      style={{
-                        width: 70,
-                        height: 70,
-                        borderRadius: 14,
-                        backgroundColor: "white",
-                      }}
-                      source={{ uri: item.image_path }}
-                    />
-                    <View>
-                      <Text
-                        numberOfLines={1}
-                        style={{
-                          color: "white",
-                          fontSize: 20,
-                          fontWeight: 500,
-                          width:200,
-                          marginBottom:4,
-                        }}
-                      >
-                        {item.name}
-                      </Text>
-                      <Text style={{color:"white"}}>{item.position?.name}</Text>
-                    </View>
-                    <View
-                      style={{
-                        marginLeft: "auto",
-                        alignItems: "center",
-                        gap: 4,
-                      }}
-                    >
-                      <Image
-                        style={{ width: 30, height: 20, borderRadius: 5 }}
-                        source={{ uri: item.country.image_path }}
-                      />
-                      <Text style={{ color: "white", fontWeight: 600 }}>
-                        {item.country.iso3 || item.country.iso2}
-                      </Text>
-                    </View>
-                  </View>
-                </TouchableOpacity>
-              )}
-              keyExtractor={(item) => item.id}
-            />
-          </>
-        ) : (
-          <>
-            <FlatList
-              data={playersSearched}
-              renderItem={({ item }) => (
-                <TouchableOpacity onPress={()=>{
-                    router.push(`player-details/${item.id}`)
-                }} style={{ marginBottom: 12 }}>
-                  <View
-                    style={{
-                      backgroundColor: "black",
-                      padding: 16,
-                      borderRadius: 12,
-                      flexDirection: "row",
-                      alignItems: "center",
-                      gap: 8,
-                    }}
-                  >
-                    <Image
-                      style={{
-                        width: 70,
-                        height: 70,
-                        borderRadius: 14,
-                        backgroundColor: "white",
-                      }}
-                      source={{ uri: item.image_path }}
-                    />
-                    <View>
-                      <Text
-                        numberOfLines={1}
-                        style={{
-                          color: "white",
-                          fontSize: 20,
-                          fontWeight: 500,
-                          width:200,
-                          marginBottom:4,
-                          flex:1
-                        }}
-                      >
-                        {item.name}
-                      </Text>
-                      <Text style={{color:"white"}}>{item.position?.name}</Text>
-                    </View>
-                    <View
-                      style={{
-                        marginLeft: "auto",
-                        alignItems: "center",
-                        gap: 4,
-                      }}
-                    >
-                      <Image
-                        style={{ width: 30, height: 20, borderRadius: 5 }}
-                        source={{ uri: item.country.image_path }}
-                      />
-                      <Text style={{ color: "white", fontWeight: 600 }}>
-                        {item.country.iso3 || item.country.iso2}
-                      </Text>
-                    </View>
-                  </View>
-                </TouchableOpacity>
-              )}
-              keyExtractor={(item) => item.id}
-            />
-          </>
-        )}
-      </ScrollView>
+      <PlayersContainer searchStatement={searchStatement} />
     </View>
   );
 };
