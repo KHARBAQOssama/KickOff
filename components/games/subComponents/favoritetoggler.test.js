d054566aa1ebe9f649582640af3172b5ddb65ef8
Ossama kharbaq <ossamakharbaq4@gmail.com>
2024-02-26 19:27:09 +0100

    Update tests for favorite toggler component in games

--- components/games/subComponents/favoritetoggler.test.js
+++ components/games/subComponents/favoritetoggler.test.js
@@ -1,12 +1,74 @@
 import React from "react";
-import { render } from "@testing-library/react-native";
-
+import { render, fireEvent } from "@testing-library/react-native";
+import { Provider } from "react-redux";
+import configureStore from "redux-mock-store";
 import FavoriteToggler from "./FavoriteToggler";
+import { toggleGameSaving } from "../../../src/features/global.slice";
 
-describe("<FavoriteToggler />", () => {
+jest.mock("react-redux", () => {
+  return {
+    useDispatch: jest.fn(),
+    useSelector: jest.fn(),
+  };
+});
+
+jest.mock("@react-native-async-storage/async-storage", () => ({
+  getItem: jest.fn(),
+  setItem: jest.fn(),
+  removeItem: jest.fn(),
+  clear: jest.fn(),
+}));
+
+const mockStore = configureStore([]);
+
+describe("FavoriteToggler", () => {
+  let store;
+
+  beforeEach(() => {
+    store = mockStore({
+      global: {
+        savedGames: ["123"],
+      },
+    });
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
   it("renders correctly", () => {
-    const { getByTestId } = render(<FavoriteToggler />);
-    const favoriteToggler = getByTestId("favorite-toggler");
-    expect(favoriteToggler).toBeDefined();
+    const { getByTestId } = render(
+      <Provider store={store}>
+        <FavoriteToggler fixtureId="123" />
+      </Provider>
+    );
+
+    expect(getByTestId("favorite-toggler")).toBeTruthy();
+  });
+
+  it("displays the correct heart icon based on saved status", () => {
+    const { getByTestId } = render(
+      <Provider store={store}>
+        <FavoriteToggler fixtureId="123" />
+      </Provider>
+    );
+
+    const heartIcon = getByTestId("heart-icon");
+    expect(heartIcon.props.source).toEqual(
+      require("../../../assets/icons/red-heart.png")
+    );
+  });
+
+  it("dispatches toggleGameSaving action when pressed", () => {
+    const { getByTestId } = render(
+      <Provider store={store}>
+        <FavoriteToggler fixtureId="456" />
+      </Provider>
+    );
+
+    const heartIcon = getByTestId("heart-icon");
+    fireEvent.press(heartIcon);
+
+    expect(store.dispatch).toHaveBeenCalledWith(toggleGameSaving("456"));
   });
 });
